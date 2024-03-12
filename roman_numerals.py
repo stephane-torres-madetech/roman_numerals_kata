cc277c6463a31e280343fe923473f722f08cc11c
Stephane Torres <stephane.torres@madetech.com>
2024-03-12 14:37:51 +0000

    test: GREEN 41 => XLI

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,5 +1,6 @@
 
 roman_numerals = {
+        0 : '',
         1 : 'I',
         2 : 'II',
         3 : 'III',
@@ -22,15 +23,21 @@ def convert_to_roman_numeral(num):
     if num <= 10 or num >= 50:
        result = roman_numerals[num]
     elif num > 10 and num <= 20:
+        result = 'X'
         units = num - 10
         result += roman_numerals[units]
     elif num > 20 and num <= 30:
+        result = 'XX'
         units = num - 20
-        result += 'X' + roman_numerals[units]
+        result += roman_numerals[units]
     elif num > 30 and num < 40:
+        result = 'XXX'
         units = num - 30
-        result += 'XX' + roman_numerals[units]
+        result += roman_numerals[units]
     else:
+        num >= 40 and num < 50
         result = 'XL'
+        units = num - 40
+        result += roman_numerals[units]
     
     return result
